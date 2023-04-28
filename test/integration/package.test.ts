bcd432cd67b8c2d4be7b2893bef73d36e33fbe47
Alex Rozanski <alex@rozanski.me>
2023-04-28 23:04:35 +0100

    simple test using swift package dump-package

--- test/integration/package.test.ts
+++ test/integration/package.test.ts
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import fs from "fs";
 import tmp, { DirResult } from "tmp";
 import { createPackage } from "../../lib/create";
@@ -7,6 +8,15 @@ describe("Package tests", () => {
 
   beforeEach(() => {
     tmpDir = tmp.dirSync({ unsafeCleanup: true });
+
+    createPackage(tmpDir.name, {
+      name: "test",
+      platforms: [{ platform: "macOS", minimumVersion: "13.0" }],
+      targetType: "library",
+      targetLanguage: "swift",
+      minimumSwiftVersion: "5.7.1",
+      includeTests: true,
+    });
   });
 
   afterEach(() => {
@@ -14,16 +24,22 @@ describe("Package tests", () => {
   });
 
   test("successfully creates Package.swift", () => {
-    createPackage(tmpDir.name, {
-      name: "test",
-      platforms: [{ platform: "macOS", minimumVersion: "" }],
-      targetType: "library",
-      targetLanguage: "swift",
-      minimumSwiftVersion: "5.5",
-      includeTests: true,
-    });
-
     const packageExists = fs.existsSync(`${tmpDir.name}/Package.swift`);
     expect(packageExists).toBeTruthy();
+  });
+
+  test("creates valid Package.swift", (done) => {
+    exec(
+      "/usr/bin/env swift package dump-package",
+      { cwd: tmpDir.name },
+      (error, stdout, stderr) => {
+        try {
+          expect(error).toBe(null);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      }
+    );
   });
 });
