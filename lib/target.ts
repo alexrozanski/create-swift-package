a9c10f35b9c46f79df54f53f28bcc105b88d3832
Alex Rozanski <alex@rozanski.me>
2023-04-29 09:49:33 +0100

    add test targets if needed

--- lib/target.ts
+++ lib/target.ts
@@ -6,12 +6,24 @@ export type Target = {
 };
 
 export const makeTargets = (config: Config): Target[] => {
+  const targets: Target[] = [];
+
+  let mainTarget: Target;
   switch (config.language) {
     case "cfamily":
     case "swift":
-      return [{ name: config.name, dependencies: [] }];
+      mainTarget = { name: config.name, dependencies: [] };
+      targets.push(mainTarget);
     case "mixed":
       const objCxx = { name: `${config.name}ObjCxx`, dependencies: [] };
-      return [{ name: config.name, dependencies: [objCxx] }, objCxx];
+      mainTarget = { name: config.name, dependencies: [objCxx] };
+      targets.push(mainTarget);
+      targets.push(objCxx);
   }
+
+  if (config.includeTests) {
+    targets.push({ name: `${config.name}Tests`, dependencies: [mainTarget] });
+  }
+
+  return targets;
 };
