94510d42df24f527155ff68b35678424648c5f6c
Alex Rozanski <alex@rozanski.me>
2023-04-29 22:40:50 +0100

    use execa for executing shell commands

--- lib/package/postActions.ts
+++ lib/package/postActions.ts
@@ -1,54 +1,50 @@
+import chalk from "chalk";
 import { exec } from "child_process";
+import { execa } from "execa";
 import fs from "fs";
 import ora from "ora";
 import path from "path";
 import { evaluateTemplate } from "../file/template";
-import { execAsync } from "../util/process";
+
+/* Git */
 
 export const initGitRepo = async (directory: string) => {
   const spinner = ora("Initializing git repo...").start();
 
   // Check git installation.
   try {
-    await execAsync("/usr/bin/env which git");
-  } catch {
+    await execa("which", ["git"]);
+  } catch (err) {
     spinner.fail(
       "Couldn't find git installation - make sure it's in your path."
     );
     return;
   }
 
-  // Run `git init`
   try {
-    await execAsync(`/usr/bin/env git init ${directory}`);
-  } catch {
-    spinner.fail();
-    return;
-  }
+    // Run `git init`
+    await execa("git", ["init", directory]);
 
-  // Write .gitignore
-  try {
+    // Write .gitignore
     await fs.promises.writeFile(
       path.join(directory, ".gitignore"),
       evaluateTemplate({ template: "gitignore", props: {} })
     );
-  } catch {
-    spinner.fail();
-  }
 
-  // Make initial commit
-  try {
-    await execAsync(`/usr/bin/env git -C ${directory} add .`);
-    await execAsync(
-      `/usr/bin/env git -C ${directory} commit -m "Initial commit"`
-    );
+    // Make initial commit
+    await execa("git", ["-C", directory, "add", "."]);
+    await execa("git", ["-C", directory, "commit", "-m", '"Initial commit"']);
   } catch {
     spinner.fail();
     return;
   }
 
-  spinner.succeed();
+  spinner.succeed(chalk.bold("Initialized git repo"));
 };
+
+/* Swift build */
+
+/* Xcode */
 
 export const openInXcode = async (directory: string) => {
   // TODO: Read value from xcselect and use that?
