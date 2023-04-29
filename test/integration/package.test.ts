37a6f5e502dbe0d676ad9d82cb30c52d8632f478
Alex Rozanski <alex@rozanski.me>
2023-04-29 12:00:34 +0100

    fix integration tests

--- test/integration/package.test.ts
+++ test/integration/package.test.ts
@@ -1,44 +1,59 @@
 import { exec } from "child_process";
-import fs from "fs";
 import path from "path";
-import tmp, { DirResult } from "tmp";
+import tmp from "tmp";
+import { type Config } from "../../lib/config";
 import { createPackage } from "../../lib/package/create";
+import { makeTargets } from "../../lib/package/target";
+import { exists } from "../../lib/util/fs";
 
 describe("Package integration tests", () => {
-  let tmpDir: DirResult;
+  let tmpDir: string;
+  let removeDir: () => void;
 
-  beforeEach(() => {
-    tmpDir = tmp.dirSync({ unsafeCleanup: true });
+  beforeEach(async () => {
+    const { path, remove } = await new Promise<{
+      path: string;
+      remove: () => void;
+    }>((resolve, reject) => {
+      tmp.dir({ unsafeCleanup: true }, (err, path, remove) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ path, remove });
+        }
+      });
+    });
 
-    createPackage({
-      config: {
-        projectDir: tmpDir.name,
-        name: "test",
-        platforms: [{ platform: "macOS", minimumVersion: "10.13" }],
-        productType: "library",
-        language: "swift",
-        minimumSwiftVersion: "5.7",
-        includeTests: true,
-      },
-      targets: [],
+    tmpDir = path;
+    removeDir = remove;
+
+    const config: Config = {
+      projectDir: tmpDir,
+      name: "test",
+      platforms: [{ platform: "macOS", minimumVersion: "10.13" }],
+      productType: "library",
+      language: "swift",
+      minimumSwiftVersion: "5.7",
+      includeTests: true,
+    };
+
+    await createPackage({
+      config,
+      targets: makeTargets(config),
     });
   });
 
-  afterEach(() => {
-    tmpDir.removeCallback();
-  });
+  afterEach(() => removeDir());
 
-  test("successfully creates Package.swift", () => {
-    const packageExists = fs.existsSync(
-      path.join(tmpDir.name, "Package.swift")
-    );
+  test("successfully creates Package.swift", async () => {
+    const packageExists = await exists(path.join(tmpDir, "Package.swift"));
     expect(packageExists).toBeTruthy();
   });
 
   test("creates valid Package.swift", (done) => {
     exec(
       "/usr/bin/env swift package dump-package",
-      { cwd: tmpDir.name },
+      { cwd: tmpDir },
       (error, stdout, stderr) => {
         try {
           expect(error).toBe(null);
